32a51b5fc9d2cbb0dd896e1a65f87d022bf792b4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 13:22:03 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -60,20 +60,20 @@ EOT
     insights = optional(object({
       default_log_analytics_workspace_id = string
       enabled                            = bool
-      log_analytics_workspace = optional(object({
+      log_analytics_workspace = optional(list(object({
         firewall_location = string
         id                = string
-      }))
+      })))
       retention_in_days = optional(number)
     }))
     intrusion_detection = optional(object({
       mode           = optional(string)
       private_ranges = optional(list(string))
-      signature_overrides = optional(object({
+      signature_overrides = optional(list(object({
         id    = optional(string)
         state = optional(string)
-      }))
-      traffic_bypass = optional(object({
+      })))
+      traffic_bypass = optional(list(object({
         description           = optional(string)
         destination_addresses = optional(set(string))
         destination_ip_groups = optional(set(string))
@@ -82,7 +82,7 @@ EOT
         protocol              = string
         source_addresses      = optional(set(string))
         source_ip_groups      = optional(set(string))
-      }))
+      })))
     }))
     threat_intelligence_allowlist = optional(object({
       fqdns        = optional(set(string))
@@ -105,15 +105,15 @@ EOT
           destination_fqdn_tags = optional(list(string))
           destination_fqdns     = optional(list(string))
           destination_urls      = optional(list(string))
-          http_headers = optional(object({
+          http_headers = optional(list(object({
             name  = string
             value = string
-          }))
+          })))
           name = string
-          protocols = optional(object({
+          protocols = optional(list(object({
             port = number
             type = string
-          }))
+          })))
           source_addresses = optional(list(string))
           source_ip_groups = optional(list(string))
           terminate_tls    = optional(bool)
